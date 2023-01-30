d55fbe07648a8ddb187e8ef0a16fdac35c918a77
witehound <netvid00@gmail.com>
2023-01-30 16:25:04 +0100

    created file to export texture

--- src/images/textures.js
+++ src/images/textures.js
@@ -1,1 +1,9 @@
 import { dirtImg, grassImg, glassImg, woodImg, logImg } from "./images";
+import { TextureLoader } from "three";
+
+export const dirtTexture = new TextureLoader(dirtImg);
+export const logTexture = new TextureLoader(logImg);
+export const grassTexture = new TextureLoader(grassImg);
+export const woodTexture = new TextureLoader(woodImg);
+export const glassTexture = new TextureLoader(glassImg);
+export const groundTexture = new TextureLoader(dirtImg);
